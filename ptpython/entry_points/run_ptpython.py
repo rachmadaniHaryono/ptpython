e74939317fd786bc1fce38d9a5e5c06fc8791a26
Rachmadani Haryono <foreturiga@gmail.com>
2022-02-21 16:36:22 +0800

    refactor(run_ptpython): use upstream fix

--- ptpython/entry_points/run_ptpython.py
+++ ptpython/entry_points/run_ptpython.py
@@ -34,10 +34,10 @@ from prompt_toolkit.shortcuts import print_formatted_text
 
 from ptpython.repl import embed, enable_deprecation_warnings, run_config
 
-if sys.version >= (3, 8):
+try:
     from importlib import metadata
-else:
-    import importlib_metadata as metadata
+except ImportError:
+    import importlib_metadata as metadata  # type: ignore
 
 
 __all__ = ["create_parser", "get_config_and_history_file", "run"]
